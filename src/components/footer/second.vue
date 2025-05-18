c30cfc0937889d4e972f128a5357d2db60dcb8e2
瑋庭 <dna11103096@gmail.com>
2025-05-19 03:03:53 +0800

    Remove css from second.vue and use bootstrap and element plus

--- src/components/footer/second.vue
+++ src/components/footer/second.vue
@@ -1,30 +1,14 @@
 <template>
-    <div class="four">
-      <div class="pattern">
+    <div class="d-flex justify-content-center my-3 px-2">
+      <el-card class="p-3" shadow="always">
         <a href="#">
-          <img :src="down" width="100%" height="100%" />
+          <img :src="down" class="img-fluid" />
         </a>
-      </div>
+      </el-card>
     </div>
 </template>
   
   <script setup lang="ts">
     import down from '@/assets/image/down.png'
   </script>
-  
-  <style scoped>
-  .four{
-        height:150px;
-        display: flex;
-        justify-content: center;  
-        margin-right: 10px;
-        margin-left: 10px;
-    }
-    .pattern{
-        display: flex;
-        justify-content: center;
-        padding: 15px;
-        background-color: #ffffff;
-    }
-  </style>
   
