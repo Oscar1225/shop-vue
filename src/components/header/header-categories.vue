8567e30bf11a911535285719e32d569a95f35617
Oscar <a076437710@gmail.com>
2025-05-18 21:59:30 +0800

    feat:main categories

--- src/components/header/header-categories.vue
+++ src/components/header/header-categories.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="d-flex flex-wrap justify-content-center gap-2 mt-2 text-white">
     <router-link :to="{ path: '/search', query: { keyword: 'phone' } }" class="text-white text-decoration-none">超便宜手機</router-link>
-    <el-link href="#" class="text-white" underline={false}>Nike官網</el-link>
-    <el-link href="#" class="text-white" underline={false}>學生裝</el-link>
-    <el-link href="#" class="text-white" underline={false}>行充20000mah</el-link>
-    <el-link href="#" class="text-white" underline={false}>假指甲貼片</el-link>
-    <el-link href="#" class="text-white" underline={false}>手飾</el-link>
-    <el-link href="#" class="text-white" underline={false}>解壓球捏捏</el-link>
-    <el-link href="#" class="text-white" underline={false}>拍立得相機</el-link>
-    <el-link href="#" class="text-white" underline={false}>軟骨頭沙發</el-link>
-    <el-link href="#" class="text-white" underline={false}>Jogging Pants Men</el-link>
-    <el-link href="#" class="text-white" underline={false}>流行包包</el-link>
+    <el-link href="#" class="text-white" underline="never">Nike官網</el-link>
+    <el-link href="#" class="text-white" underline="never">學生裝</el-link>
+    <el-link href="#" class="text-white" underline="never">行充20000mah</el-link>
+    <el-link href="#" class="text-white" underline="never">假指甲貼片</el-link>
+    <el-link href="#" class="text-white" underline="never">手飾</el-link>
+    <el-link href="#" class="text-white" underline="never">解壓球捏捏</el-link>
+    <el-link href="#" class="text-white" underline="never">拍立得相機</el-link>
+    <el-link href="#" class="text-white" underline="never">軟骨頭沙發</el-link>
+    <el-link href="#" class="text-white" underline="never">Jogging Pants Men</el-link>
+    <el-link href="#" class="text-white" underline="never">流行包包</el-link>
   </div>
 </template>
-
-<script setup lang="ts">
-// 無需額外邏輯
-</script>
