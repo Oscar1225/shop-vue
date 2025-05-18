5c4718f0c9b934b31afdf0a7a7062126d5f8fedd
Oscar <a076437710@gmail.com>
2025-05-18 22:07:09 +0800

    feat:header cat

--- src/components/header/header-categories.vue
+++ src/components/header/header-categories.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="d-flex flex-wrap justify-content-center gap-2 mt-2 text-white">
     <router-link :to="{ path: '/search', query: { keyword: 'phone' } }" class="text-white text-decoration-none">超便宜手機</router-link>
-    <el-link href="#" class="text-white" underline="never">Nike官網</el-link>
-    <el-link href="#" class="text-white" underline="never">學生裝</el-link>
-    <el-link href="#" class="text-white" underline="never">行充20000mah</el-link>
-    <el-link href="#" class="text-white" underline="never">假指甲貼片</el-link>
-    <el-link href="#" class="text-white" underline="never">手飾</el-link>
-    <el-link href="#" class="text-white" underline="never">解壓球捏捏</el-link>
-    <el-link href="#" class="text-white" underline="never">拍立得相機</el-link>
-    <el-link href="#" class="text-white" underline="never">軟骨頭沙發</el-link>
-    <el-link href="#" class="text-white" underline="never">Jogging Pants Men</el-link>
-    <el-link href="#" class="text-white" underline="never">流行包包</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;Nike官網</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;學生裝</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;行充20000mah</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;假指甲貼片</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;手飾</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;解壓球捏捏</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;拍立得相機</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;軟骨頭沙發</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;Jogging Pants Men</el-link>
+    <el-link href="#" class="text-white" underline="never">|&nbsp;&nbsp;流行包包</el-link>
   </div>
 </template>
