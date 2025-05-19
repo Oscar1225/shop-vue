77d9f3e3b9d6c7186be14c5509933b7c394da03f
Oscar <a076437710@gmail.com>
2025-05-19 21:36:51 +0800

    feat:customKYCpage

--- src/components/main/AccountPages/CustomKYCPage.vue
+++ src/components/main/AccountPages/CustomKYCPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="d-flex mx-3 mt-3">
+  <div class="d-flex mx-4 mt-3">
     <!-- 左側導覽欄 -->
     <AssidePage />
 
     <!-- 右側內容區 -->
     <main
-      class="ms-4 flex-grow-1 p-4 bg-white shadow d-flex justify-content-center align-items-center"
+      class="ms-4 flex-grow-1 p-4 bg-white shadow justify-content-center align-items-center d-flex"
       style="height: 650px;"
     >
-      <el-text tag="h1" class=" fw-bold text-center" style="font-size: 2rem;">關務署實名認證</el-text>
+      <el-text tag="h2" class=" fw-bold text-center" style="font-size: 2rem;">關務署實名認證</el-text>
     </main>
   </div>
 </template>
