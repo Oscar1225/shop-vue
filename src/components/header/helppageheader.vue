11b0586202e788fb78a06812e72cc705114a52aa
Oscar <a076437710@gmail.com>
2025-05-25 21:04:55 +0800

    style:helppagegeader

--- src/components/header/helppageheader.vue
+++ src/components/header/helppageheader.vue
@@ -3,7 +3,7 @@
     <div class="container d-flex justify-content-between align-items-center py-2" style="height:60px">
       <router-link to="/help" class="d-flex align-items-center text-decoration-none">
         <i class="fa-solid fa-bag-shopping me-2" style="font-size: 30px; color: #FF7043;"></i>
-        <span class="help-center-text ms-1 fw-bold fs-5 " style="color: #FF7043; ">Shopee | 蝦皮幫助中心</span>
+        <span class=" ms-1 fw-bold fs-5 " style="color: #FF7043; ">Shopee | 蝦皮幫助中心</span>
       </router-link>
       <div class=" fw-bold fs-5" style="color: #FF7043;">蝦皮政策</div>
     </div>
@@ -14,8 +14,4 @@
 </script>
 
 <style scoped>
-.help-center-text {
-  color: white;
-  font-weight: 500;
-}
 </style>
