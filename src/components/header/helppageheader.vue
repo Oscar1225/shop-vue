bfa11f38b83f04e165e182dc939c702df44de998
Oscar <a076437710@gmail.com>
2025-05-25 18:42:03 +0800

    feat:helppageheader

--- src/components/header/helppageheader.vue
+++ src/components/header/helppageheader.vue
@@ -2,10 +2,10 @@
   <div class="shopee-header">
     <div class="container d-flex justify-content-between align-items-center py-2" style="height:60px">
       <router-link to="/" class="d-flex align-items-center text-decoration-none">
-        <i class="fa-solid fa-bag-shopping me-2 text-danger" style="font-size: 30px;"></i>
-        <span class="help-center-text ms-1 fw-bold">蝦皮幫助中心</span>
+        <i class="fa-solid fa-bag-shopping me-2 text-white" style="font-size: 30px;"></i>
+        <span class="help-center-text ms-1 fw-bold fs-5">蝦皮幫助中心</span>
       </router-link>
-      <div class="text-muted">蝦皮政策</div>
+      <div class="text-white fw-bold fs-5">蝦皮政策</div>
     </div>
   </div>
 </template>
@@ -16,11 +16,10 @@
 
 <style scoped>
 .shopee-header {
-  background-color: white;
-  border-bottom: 4px solid #ee4d2d;
+  background: blue
 }
 .help-center-text {
-  color: #FF7043;
+  color: white;
   font-weight: 500;
 }
 </style>
