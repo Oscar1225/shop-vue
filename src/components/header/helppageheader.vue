0825618d1ff7f562eea83232e20c42382b173ceb
Oscar <a076437710@gmail.com>
2025-05-25 21:01:32 +0800

    style:helppageheader

--- src/components/header/helppageheader.vue
+++ src/components/header/helppageheader.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="shopee-header">
+  <div class="bg-light">
     <div class="container d-flex justify-content-between align-items-center py-2" style="height:60px">
       <router-link to="/" class="d-flex align-items-center text-decoration-none">
-        <i class="fa-solid fa-bag-shopping me-2 text-white" style="font-size: 30px;"></i>
-        <span class="help-center-text ms-1 fw-bold fs-5">蝦皮幫助中心</span>
+        <i class="fa-solid fa-bag-shopping me-2" style="font-size: 30px; color: #FF7043;"></i>
+        <span class="help-center-text ms-1 fw-bold fs-5 " style="color: #FF7043; ">Shopee | 蝦皮幫助中心</span>
       </router-link>
-      <div class="text-white fw-bold fs-5">蝦皮政策</div>
+      <div class=" fw-bold fs-5" style="color: #FF7043;">蝦皮政策</div>
     </div>
   </div>
 </template>
 
 <script setup>
-// 無需 JS
 </script>
 
 <style scoped>
-.shopee-header {
-  background: blue
-}
 .help-center-text {
   color: white;
   font-weight: 500;
