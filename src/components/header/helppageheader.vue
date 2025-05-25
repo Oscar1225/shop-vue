15cb3a40805015567f0af08645988c74d68d0e2e
Oscar <a076437710@gmail.com>
2025-05-25 21:03:29 +0800

    feat:helppage

--- src/components/header/helppageheader.vue
+++ src/components/header/helppageheader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-light">
     <div class="container d-flex justify-content-between align-items-center py-2" style="height:60px">
-      <router-link to="/" class="d-flex align-items-center text-decoration-none">
+      <router-link to="/help" class="d-flex align-items-center text-decoration-none">
         <i class="fa-solid fa-bag-shopping me-2" style="font-size: 30px; color: #FF7043;"></i>
         <span class="help-center-text ms-1 fw-bold fs-5 " style="color: #FF7043; ">Shopee | 蝦皮幫助中心</span>
       </router-link>
